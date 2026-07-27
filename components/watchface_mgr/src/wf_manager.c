20f541cf9faca5158c4f929c1f7e57a600922d44
Sourav Kanta <sourav.kanta@gmail.com>
2026-07-27 18:06:52 +0530

    Added infrastructure for watchface persistance - Watchface manager now queries the storage manager to select the watchface - Incase of no watchface selected it defaults to wf index 0

--- components/watchface_mgr/src/wf_manager.c
+++ components/watchface_mgr/src/wf_manager.c
@@ -6,36 +6,71 @@
 #include <common_types.h>
 #include <lvgl.h>
 #include <common_apis.h>
+#include <storage_manager.h>
 
 #include <wf_abstract_dark.h>
 #include <wf_analog.h>
 
 static watchface_t* all_wfs[MAX_WATCHFACES];
-static watchface_t* selected_wf = NULL;
-static watchface_t* curr_wf = NULL;
+static const watchface_t* selected_wf = NULL;
+static const watchface_t* curr_wf = NULL;
 static const char* TAG = "Watchface Manager";
 static bool initialized = false;
 
 static int num_wfs=0;
 
 static void register_wf(watchface_t* wf) {
+    if(!initialized) return;
     if(num_wfs >= MAX_WATCHFACES) {
         return;
     }
     all_wfs[num_wfs] = wf;
+    assert(all_wfs[num_wfs]);
     ++num_wfs;
     ESP_LOGI(TAG, "Adding watchfaces : %s", wf->name);
 }
 
-watchface_t* select_wf() {
-    
+bool watchface_manager_select_wf(uint8_t idx) {
+    if(!initialized) return false;
     ESP_LOGI(TAG, "No of watchfaces : %d", num_wfs);
-    if(num_wfs == 0) {
-        ESP_LOGE(TAG, "No watchfaces present");
+    if(num_wfs == 0 || num_wfs <= idx) {
+        ESP_LOGE(TAG, "Invalid watchface selection");
+        return false;
     }
 
-    // @todo get from settings
-    selected_wf = all_wfs[0];
+    bool success = storage_manager_save_key(WATCHFACE_SYSTEM_APP_ID, "WF_IDX", &idx, sizeof(uint8_t));
+    if(!success) {
+        ESP_LOGE(TAG, "Failed to store the selected watchface");
+    }
+    return success;
+}
+
+const watchface_t* watchface_manager_get_selected_wf(void) {
+    if(!initialized) return NULL;
+    if(selected_wf == NULL) {
+        uint8_t idx;
+        uint8_t data_len = sizeof(uint8_t);
+        bool success = storage_manager_retrieve_key(WATCHFACE_SYSTEM_APP_ID, "WF_IDX", &idx, &data_len);
+        if(success) {
+            assert(data_len == sizeof(uint8_t));
+            if(idx >= num_wfs) {
+                ESP_LOGE(TAG, "The selected watchface is no longer available. Fallback to default");
+                idx = 0;
+            }
+            selected_wf = all_wfs[idx];
+            assert(selected_wf);
+        }
+        else {
+            // No watchface selection, first time use case
+            // Use the first one available
+            uint8_t idx = 0;
+            selected_wf = all_wfs[idx];
+            assert(selected_wf);
+            // Store selected watchface in flash memory
+            watchface_manager_select_wf(idx);
+        }
+    }
+    ESP_LOGI(TAG, "Selecting watchface : %s", selected_wf->name);
     return selected_wf;
 }
 
@@ -47,16 +82,17 @@ void watchface_manager_init(void) {
     initialized = true;
     register_wf(get_analog_wf());
     register_wf(get_abstract_dark_wf());
-
-    select_wf();
 }
 
 void watchface_manager_deinit(void) {
     num_wfs = 0;
     initialized = false;
+    curr_wf = NULL;
+    selected_wf = NULL;
 }
 
 static void dispatch_wf_update(const event_t *event) {
+    if(!initialized) return;
     if(!curr_wf) return;
     date_time_t date;
     hourly_weather_t weather[24];
@@ -66,10 +102,13 @@ static void dispatch_wf_update(const event_t *event) {
         .time = date,
         .weather = weather[date.hr]
     };
+    assert(curr_wf);
+    assert(curr_wf->update_watchface);
     curr_wf->update_watchface(&payload);
 }
 
 void watchface_manager_suspend(void) {
+    if(!initialized) return;
     if (!curr_wf)
         return;
     event_unsubscribe(EVENT_WATCHFACE_UPDATE, dispatch_wf_update);
@@ -77,6 +116,7 @@ void watchface_manager_suspend(void) {
 }
 
 void watchface_manager_resume(void) {
+    if(!initialized) return;
     if (!curr_wf)
         return;
     event_subscribe(EVENT_WATCHFACE_UPDATE, dispatch_wf_update);
@@ -84,16 +124,17 @@ void watchface_manager_resume(void) {
 }
 
 void watchface_manager_start_wf(lv_obj_t *parent) {
-    if(!selected_wf) return;
-    curr_wf = selected_wf;
-    ESP_LOGI(TAG, "Selected watchface : %s",
-            curr_wf->name);
+    if(!initialized) return;
+    curr_wf = watchface_manager_get_selected_wf();
+    assert(curr_wf);
+    assert(curr_wf->draw_watchface);
     curr_wf->draw_watchface(parent);
     event_subscribe(EVENT_WATCHFACE_UPDATE, dispatch_wf_update);
     tick_manager_generate_tick(TICK_WATCHFACE);
 }
 
 void watchface_manager_stop_wf(void) {
+    if(!initialized) return;
     if(!curr_wf) {
         ESP_LOGE(TAG, "Unknown watchface to stop");
         return;
